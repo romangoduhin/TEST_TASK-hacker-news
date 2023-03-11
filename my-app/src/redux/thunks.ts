6547b85d63913a88f9047ad5fb15330e630b16a5
Roman Goduhin <superchewits2000@gmail.com>
2023-03-11 03:24:25 +0300

    fix: implement notification

--- my-app/src/redux/thunks.ts
+++ my-app/src/redux/thunks.ts
@@ -1,22 +1,28 @@
 import {Dispatch} from '@reduxjs/toolkit';
 import newsAPI from "../services/newsApi";
 import {setNewsIds} from "./slices/newsSlice";
+import {notifications} from "@mantine/notifications";
 
 export const getNewsIdsThunk = () => async (dispatch: Dispatch) => {
     const arrSize = 100;
 
     const storyIds = await newsAPI.getNewsIds();
-    const slicedStoryIds = storyIds.slice(0,arrSize)
+    const slicedStoryIds = storyIds.slice(0, arrSize)
 
-    if(!slicedStoryIds) return;
+    if (!slicedStoryIds) return;
 
     dispatch(setNewsIds(slicedStoryIds));
+
+    notifications.show({
+        title: 'News updated',
+        message: ''
+    })
 };
 
-export const getNewsByIdThunk = async (id: number)  => {
+export const getNewsByIdThunk = async (id: number) => {
     const story = await newsAPI.getNewsById(id);
 
-    if(!story) return;
+    if (!story) return;
 
     return story;
 };
